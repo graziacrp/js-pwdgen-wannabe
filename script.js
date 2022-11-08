bee28c500428838957c0db25b6d6c7de880b1b53
graziacrp <graziacarpineta25@gmail.com>
2022-11-08 14:05:11 +0100

    ok

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+let nome = prompt ("Inserisci il nome");
+let cognome = prompt ("Inserisci il cognome");
+let colore preferito = prompt ("Inserisci il colore preferito");
+
+
+console.log (nome);
+console.log(cognome);
+console.log (colore preferito);
+
+
+
+
+
+
+console.log {
+
+    ` 
+    
+
+    ----- Informazioni di contatto -----
+    Password: $ {nomecognomecolorepreferito21};
+
+   
+
+    `  
+};
